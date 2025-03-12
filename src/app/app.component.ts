7e1ed63babfe946d9e84bd6b009e886b05fd6c18
Shivam jain <shivamjain946565@gmail.com>
2025-03-12 17:21:41 +0530

    To-Do-App Project
    Add functionality of add, update and delete the tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'to-do-app';
+  val: string = '';
+  task: string[] = [];
+  editInd: number = -1;
+  editVal: string = '';
+
+  addTask() {
+    if (this.val.trim()) {
+      this.task.push(this.val.trim());
+      this.val = '';
+    }
+  }
+
+  deleteTask(ind: number) {
+    this.task.splice(ind, 1);
+  }
+
+  editTask(ind: number) {
+    this.editInd = ind;
+    this.editVal = this.task[ind];
+  }
+
+  save() {
+    if (this.editVal.trim() && this.editInd !== -1) {
+      this.task[this.editInd] = this.editVal.trim();
+      this.editInd = -1;
+    }
+  }
+
+  cancel() {
+    this.editInd = -1;
+  }
+
+  trackByIndex(ind: number) {
+    return ind;
+  }
 }
